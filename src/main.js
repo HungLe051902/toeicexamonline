670aa6f5546baf9026dfaefe4e2a033dee121634
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2020-11-14 16:27:48 +0700

    Import thư viện element ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import mixin from './mixins'
+import 'element-ui/lib/theme-chalk/index.css';
 
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+const baseURL = "http://toeicexamonline.gq/api";
 export default {
     // Đăng nhập
     async login(user) {
         try {
-            return await axios.post(`http://toeicexamonline.gq/api/Login/login`,
+            return await axios.post(`${baseURL}/Login/login`,
                 user);
         } catch (error) {
             console.log(error);
@@ -12,7 +13,7 @@ export default {
     // Đăng ký
     async register(user) {
         try {
-            return await axios.post(`http://toeicexamonline.gq/api/Login/register`,
+            return await axios.post(`${baseURL}/api/Login/register`,
                 user);
         } catch (error) {
             console.log(error);
