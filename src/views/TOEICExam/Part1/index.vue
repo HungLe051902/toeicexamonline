6c1267d426b3a08f258bbfdcbe8598dbcbdc4f5b
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2020-11-23 17:04:33 +0700

    Thêm hiển thị câu trả lời đúng

--- src/views/TOEICExam/Part1/index.vue
+++ src/views/TOEICExam/Part1/index.vue
@@ -12,26 +12,61 @@
 
         <img :src="item.LinkImg" class="w-100 my-3" alt="" />
         <div class="option-area">
-          <label class="radio-inline mr-3"><input type="radio" :name="item.QuestionID" />&nbsp;A</label>
-          <label class="radio-inline mr-3"><input type="radio" :name="item.QuestionID" />&nbsp;B</label>
-          <label class="radio-inline mr-3"><input type="radio" :name="item.QuestionID" />&nbsp;C</label>
-          <label class="radio-inline"><input type="radio" :name="item.QuestionID" />&nbsp;D</label>
+          <label class="radio mr-3" v-bind:class="isFinished ? 'd-block' : ''"
+            ><input type="radio" :name="item.QuestionID" value="A" />&nbsp;A
+            <span
+              v-if="isFinished"
+              v-bind:class="item.Answer == 'A' ? 'font-weight-bold' : ''"
+              >. {{ item.OptionA }}</span
+            ></label
+          >
+          <label class="radio mr-3" v-bind:class="isFinished ? 'd-block' : ''"
+            ><input type="radio" :name="item.QuestionID" value="B" />&nbsp;B
+            <span
+              v-if="isFinished"
+              v-bind:class="item.Answer == 'B' ? 'font-weight-bold' : ''"
+              >. {{ item.OptionB }}</span
+            ></label
+          >
+          <label class="radio mr-3" v-bind:class="isFinished ? 'd-block' : ''"
+            ><input type="radio" :name="item.QuestionID" value="C" />&nbsp;C
+            <span
+              v-if="isFinished"
+              v-bind:class="item.Answer == 'C' ? 'font-weight-bold' : ''"
+              >. {{ item.OptionC }}</span
+            ></label
+          >
+          <label class="radio" v-bind:class="isFinished ? 'd-block' : ''"
+            ><input type="radio" :name="item.QuestionID" value="D" />&nbsp;D
+            <span
+              v-if="isFinished"
+              v-bind:class="item.Answer == 'D' ? 'font-weight-bold' : ''"
+              >. {{ item.OptionD }}</span
+            ></label
+          >
         </div>
       </div>
     </div>
-    <button v-on:click="nextToPart2" class="btn h-btn-primary mb-4">Next</button>
+    <button v-on:click="nextToPart2" class="btn h-btn-primary mb-4">
+      Next
+    </button>
+    <button v-on:click="finish" class="btn h-btn-primary mb-4 ml-3">
+      Chấm điểm
+    </button>
   </div>
 </template>
 <script>
 // import ToeicExamService from "@/services/toeicExamService.js";
 import titleResource from "@/assets/resources/title.js";
 import { mapGetters } from "vuex";
+import $ from "jquery";
 export default {
   data() {
     return {
       selectedExam: null,
       // part1Data: null,
       isShowLoading: false,
+      isFinished: false,
     };
   },
   props: {},
@@ -44,18 +79,49 @@ export default {
     this.getQuestionPart1();
   },
   methods: {
+    // test
+    finish() {
+      try {
+        var me = this;
+        this.isFinished = true;
+        var index = 0;
+        $.each($("#part1-detail .option-area"), function () {
+          var vm = this;
+          $.each($(vm).find("input"), function () {
+            if ($(this).val() == me.getPart1Data[index]?.Answer) {
+              $(this)
+                .parent()
+                .after("<span class='text-success'>Correct</span>");
+            } else {
+              if ($(this).is(":checked")) {
+                $(this)
+                  .parent()
+                  .after("<span class='text-danger'>Wrong</span>");
+              }
+            }
+          });
+          index++;
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    },
     // Lấy câu hỏi part 1
     async getQuestionPart1() {
       try {
-        if (this.getPart1Data && this.getPart1Data.length > 0){
-          return ;
+        if (this.getPart1Data && this.getPart1Data.length > 0) {
+          return;
         }
         this.isShowLoading = true;
         // var res = await ToeicExamService.getQuestionPart1ByYearAndExamNo(
         //   this.selectedExam?.Year,
         //   this.selectedExam?.ExamCode
         // );
-        var res = await this.$store.dispatch("toeicexam/getQuestionByPart", { part: "PART_1", year: this.selectedExam?.Year, examNo: this.selectedExam?.ExamCode });
+        var res = await this.$store.dispatch("toeicexam/getQuestionByPart", {
+          part: "PART_1",
+          year: this.selectedExam?.Year,
+          examNo: this.selectedExam?.ExamCode,
+        });
         this.isShowLoading = false;
         // if (res) {
         //   if (res.data.APPCode == 200) {
@@ -76,7 +142,9 @@ export default {
     // Chuyển sang phần 2
     nextToPart2() {
       try {
-        this.$router.push(`/toeicexam/${this.selectedExam?.ExamID}/part2-instruction`);
+        this.$router.push(
+          `/toeicexam/${this.selectedExam?.ExamID}/part2-instruction`
+        );
       } catch (e) {
         console.log(e);
       }
@@ -86,14 +154,6 @@ export default {
   computed: {
     ...mapGetters("toeicexam", ["getPart1Data"]),
   },
-  // watch: {
-  //   part1Data: {
-  //     deep: true,
-  //     handler(val){
-  //       console.log('part1Data', val);
-  //     }
-  //   }
-  // }
 };
 </script>
 <style lang="scss" scoped>
