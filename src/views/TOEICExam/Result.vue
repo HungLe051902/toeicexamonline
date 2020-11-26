b1e24774073c584f5e57b839f76d56c06ed2b55a
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2020-11-26 07:32:10 +0700

    Thêm button hiển thị chi tiết đáp án

--- src/views/TOEICExam/Result.vue
+++ src/views/TOEICExam/Result.vue
@@ -3,33 +3,68 @@
     <div class="w-100 h-100 bg-light p-3">
       <h3>KẾT QUẢ</h3>
       <div v-if="result" class="">
-        <div class="d-flex">
-          <div class="title">PART I:</div>
-          <div>{{result.part1Score}}/6</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART I:</div>
+            <div>{{ result.part1Score }}/6</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
-        <div class="d-flex">
-          <div class="title">PART II:</div>
-          <div>{{result.part2Score}}/25</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART II:</div>
+            <div>{{ result.part2Score }}/25</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
-        <div class="d-flex">
-          <div class="title">PART III:</div>
-          <div>{{result.part3Score}}/39</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART III:</div>
+            <div>{{ result.part3Score }}/39</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
-        <div class="d-flex">
-          <div class="title">PART IV:</div>
-          <div>{{result.part4Score}}/30</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART IV:</div>
+            <div>{{ result.part4Score }}/30</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
-        <div class="d-flex">
-          <div class="title">PART V:</div>
-          <div>{{result.part5Score}}/30</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART V:</div>
+            <div>{{ result.part5Score }}/30</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
-        <div class="d-flex">
-          <div class="title">PART VI:</div>
-          <div>{{result.part6Score}}/16</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART VI:</div>
+            <div>{{ result.part6Score }}/16</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
-        <div class="d-flex">
-          <div class="title">PART VII:</div>
-          <div>{{result.part7Score}}/54</div>
+        <div
+          class="d-flex w-50 justify-content-between align-items-center mb-2"
+        >
+          <div class="d-flex">
+            <div class="title">PART VII:</div>
+            <div>{{ result.part7Score }}/54</div>
+          </div>
+          <button class="btn h-btn-primary">Chi tiết</button>
         </div>
       </div>
     </div>
@@ -39,11 +74,13 @@
 export default {
   created() {
     // Lấy thông tin kết quả trong localStorage
-    this.result = localStorage.getItem('result') ? JSON.parse(localStorage.getItem('result')) : null;
+    this.result = localStorage.getItem("result")
+      ? JSON.parse(localStorage.getItem("result"))
+      : null;
   },
   data() {
     return {
-        result: null
+      result: null,
     };
   },
 };
