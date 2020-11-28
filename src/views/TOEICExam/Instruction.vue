dc1dae21ae0ed59647bfbd115ad7b56d688d87bf
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2020-11-28 23:25:26 +0700

    Sửa lại thời gian làm bài

--- src/views/TOEICExam/Instruction.vue
+++ src/views/TOEICExam/Instruction.vue
@@ -22,7 +22,7 @@ export default {
         if (!localStorage.getItem("timeEnd"))
           localStorage.setItem(
             "timeEnd",
-            new Date().getTime() + 120 * 1000
+            new Date().getTime() + 2 * 60 * 60 * 1000
           );
           //2 * 60 * 60 * 1000
         // Lưu trạng thái đang làm bài vào localStorage
